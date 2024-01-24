fc1155006f7aab491dd06d250f7aaa4f22dabd66
Karan-Palan <karanpalan007@gmail.com>
2024-01-24 11:23:40 +0530

    Navbar,Hero,Features and Footer section done

---
+++ src/constants/index.js
@@ -0,0 +1,178 @@
+import { people01, people02, people03, facebook, instagram, linkedin, twitter, airbnb, binance, coinbase, dropbox, send, shield, star  } from "../assets";
+
+export const navLinks = [
+  {
+    id: "home",
+    title: "Home",
+  },
+  {
+    id: "features",
+    title: "Features",
+  },
+  {
+    id: "product",
+    title: "Product",
+  },
+  {
+    id: "clients",
+    title: "Clients",
+  },
+];
+
+export const features = [
+  {
+    id: "feature-1",
+    icon: star,
+    title: "Comprehensive Vision:",
+    content:
+      "A 180-degree field of view ensures users have a holistic understanding of their surroundings.  ",
+  },
+  {
+    id: "feature-2",
+    icon: shield,
+    title: "Cloud Database Integration:",
+    content:
+      "Stores data about frequently visited areas and 10 streets near the user's home.",
+  },
+  {
+    id: "feature-3",
+    icon: send,
+    title: "Affordable Design",
+    content:
+      "Optimizes manufacturing processes and materials for cost-effectiveness.",
+  },
+];
+
+export const feedback = [
+  {
+    id: "feedback-1",
+    content:
+      "Unlike competitors, our product integrates advanced machine learning algorithms that continuously adapt, delivering more accurate and context-aware assistance, making it a smarter and more intuitive solution.",
+    name: "Cutting-Edge Machine Learning",
+    title: "Founder & Leader",
+    
+  },
+  {
+    id: "feedback-2",
+    content:
+      "Money makes your life easier. If you're lucky to have it, you're lucky.",
+    name: "Steve Mark",
+    title: "Founder & Leader",
+    img: people02,
+  },
+  {
+    id: "feedback-3",
+    content:
+      "It is usually people in the money business, finance, and international trade that are really rich.",
+    name: "Kenn Gallagher",
+    title: "Founder & Leader",
+    img: people03,
+  },
+];
+
+export const stats = [
+  {
+    id: "stats-1",
+    title: "User Active",
+    value: "3800+",
+  },
+  {
+    id: "stats-2",
+    title: "Trusted by Company",
+    value: "230+",
+  },
+  {
+    id: "stats-3",
+    title: "Transaction",
+    value: "$230M+",
+  },
+];
+
+export const  footerLinks = [
+  {
+    title: "Useful Links",
+    links: [
+      {
+        name: "Content",
+        link: "#",
+      },
+      {
+        name: "How it Works",
+        link: "#  ",
+      },
+      
+    ],
+  },
+  {
+    title: "Community",
+    links: [
+      {
+        name: "Help Center",
+        link: "#mytwitter",
+      },
+      {
+        name: "Partners",
+        link: "#link",
+      },
+      {
+        name: "Suggestions",
+        link: "#gmail",
+      },
+      {
+        name: "Blog",
+        link: "#",
+      },
+    ],
+  },
+  {
+    title: "Partner",
+    links: [
+      {
+        name: "Become a Partner",
+        link: "#",
+      },
+    ],
+  },
+];
+
+export const socialMedia = [
+  {
+    id: "social-media-1",
+    icon: instagram,
+    link: "https://www.instagram.com/",
+  },
+  {
+    id: "social-media-2",
+    icon: facebook,
+    link: "https://www.facebook.com/",
+  },
+  {
+    id: "social-media-3",
+    icon: twitter,
+    link: "https://www.twitter.com/",
+  },
+  {
+    id: "social-media-4",
+    icon: linkedin,
+    link: "https://www.linkedin.com/",
+  },
+];
+
+export const clients = [
+  {
+    id: "client-1",
+    logo: airbnb,
+  },
+  {
+    id: "client-2",
+    logo: binance,
+  },
+  {
+    id: "client-3",
+    logo: coinbase,
+  },
+  {
+    id: "client-4",
+    logo: dropbox,
+  },
+];
